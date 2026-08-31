5c554707515f5f1a881b88ec72b8fe531829bfea
map_boda <adigunpaulayotomiwa@gmail.com>
2026-08-31 13:56:50 +0000

    Add Flask dependencies

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return jsonify({
+        "success": True,
+        "message": "MAP_Technologies backend is online 🚀"
+    })
+
+
+@app.route("/api/health")
+def health():
+    return jsonify({
+        "success": True,
+        "status": "healthy"
+    })
+
+
+if __name__ == "__main__":
+    app.run()
